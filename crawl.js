eb10156e3ee338922dcf369c604343cd60d32c4c
jamesfulreader <jamesfulreader@gmail.com>
2024-07-20 15:54:48 -0600

    refactor crawl.js and crawl.test.js to test single urls instead of list and map through

--- crawl.js
+++ crawl.js
@@ -1,17 +1,24 @@
-const normalizeURL = (urls) => {
+const normalizeURL = (url) => {
   // normalizeURL() function to map all of those same inputs
   // to a single normalized output: blog.boot.dev/path
   // going to be used to compare URLs to see if they are the same page.*
 
-  const normalizedURL = urls.map((url) => {
-    let urlObj = new URL(url)
-    let fullPath = urlObj.hostname + urlObj.pathname
-    if (fullPath.slice(-1) === '/') {
-      fullPath = fullPath.slice(0, -1)
-    }
-    return fullPath
-  })
-  return normalizedURL
+  // const normalizedURL = urls.map((url) => {
+  //   let urlObj = new URL(url)
+  //   let fullPath = urlObj.hostname + urlObj.pathname
+  //   if (fullPath.slice(-1) === '/') {
+  //     fullPath = fullPath.slice(0, -1)
+  //   }
+  //   return fullPath
+  // })
+  // return normalizedURL
+  // refactor the code to take in a single URL rather than a list
+  const urlObj = new URL(url)
+  let fullPath = urlObj.hostname + urlObj.pathname
+  if (fullPath.slice(-1) === '/') {
+    fullPath = fullPath.slice(0, -1)
+  }
+  return fullPath
 }
 
 export { normalizeURL }
--- crawl.test.js
+++ crawl.test.js
@@ -1,20 +1,54 @@
 import { test, expect } from '@jest/globals'
 import { normalizeURL } from './crawl'
 
-const urls = [
-  'https://blog.boot.dev/path/',
-  'https://blog.boot.dev/path',
-  'http://blog.boot.dev/path/',
-  'http://blog.boot.dev/path',
-]
+// const urls = [
+//   'https://blog.boot.dev/path/',
+//   'https://blog.boot.dev/path',
+//   'http://blog.boot.dev/path/',
+//   'http://blog.boot.dev/path',
+//   'https://BLOG.boot.dev/path',
+//   'http://blog.BOOT.dev/path/',
+// ]
 
-const expected = [
-  'blog.boot.dev/path',
-  'blog.boot.dev/path',
-  'blog.boot.dev/path',
-  'blog.boot.dev/path',
-]
-const result = normalizeURL(urls)
-test('normalize URL', () => {
-  expect(result).toEqual(expected)
+// const expected = [
+//   'blog.boot.dev/path',
+//   'blog.boot.dev/path',
+//   'blog.boot.dev/path',
+//   'blog.boot.dev/path',
+//   'blog.boot.dev/path',
+//   'blog.boot.dev/path',
+// ]
+// const result = normalizeURL(urls)
+// test('normalize URL', () => {
+//   expect(result).toEqual(expected)
+// })
+
+// refactor code to take in single URL than a list that maps through the URLs
+
+test('normalizeURL protocol', () => {
+  const input = 'https://blog.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('normalizeURL slash', () => {
+  const input = 'https://blog.boot.dev/path/'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+  const input = 'https://BLOG.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('normalizeURL http', () => {
+  const input = 'http://BLOG.boot.dev/path'
+  const actual = normalizeURL(input)
+  const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
 })
