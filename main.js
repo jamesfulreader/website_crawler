83a9ee844c455c86e99ad0fd7b326e58e01b6548
jamesfulreader <jamesfulreader@gmail.com>
2024-07-21 12:01:04 -0600

    add cli arg input processing into main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+import { argv } from 'node:process'
 const main = () => {
-  // Todo
   // take in BASE_URL arg from cli
   // if num of cli args < 1 print error EXIT
-  // if num of cli args > 1 print error EXIT
+  if (argv.length < 3) {
+    console.log('Please enter a URL')
+    return
+    // if num of cli args > 1 print error EXIT
+  } else if (argv.length > 3) {
+    console.log('Please enter only 1 URL')
+    return
+  }
   // if num cli args === 1 baseURL = arg print "processing now. . ."
+  const baseURL = process.argv[2]
+  console.log('processing your request now. . .')
 }
 
 main()
